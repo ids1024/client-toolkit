c68efc37dfbb4a2a79f278aa7ec602a2c7b21a4f
ids1024 <ids1024@users.noreply.github.com>
2023-09-18 19:39:50 +0000

    deploy: 585f754cf4584fc9a9cd1a30b3b7f161af598101

---
+++ smithay_client_toolkit/seat/keyboard/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["KeyboardError","RepeatInfo"],"mod":["repeat"],"struct":["KeyEvent","KeyboardData","Keymap","Keysym","Modifiers","RMLVO"],"trait":["KeyboardDataExt","KeyboardHandler"]};
